e767d0d5ccea08cf06ec0213aade881aa4eea48c
Ali Topbaş <alitopbas10@gmail.com>
2025-07-05 17:13:17 +0300

    Database setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
-app.listen(5001, () => console.log('Ses ver adana'));
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello from server'));
+app.listen(ENV.PORT, () => console.log('Ses ver adana PORT: ', ENV.PORT));
