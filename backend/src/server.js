7f2f7a982985ed663e4cfaa901969c08dc323548
Ali Topbaş <alitopbas10@gmail.com>
2025-07-05 21:45:34 +0300

    connectDB function wrapped with try catch

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,21 @@ import { connectDB } from './config/db.js';
 
 const app = express();
 
-connectDB();
-
 app.get('/', (req, res) => res.send('Hello from server'));
-app.listen(ENV.PORT, () => console.log('Ses ver adana PORT: ', ENV.PORT));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    if (ENV.NODE_ENV !== 'production') {
+      app.listen(ENV.PORT, () =>
+        console.log('Server is up and running on PORT:', ENV.PORT)
+      );
+    }
+  } catch (error) {
+    console.error('Failed to start server:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
