9a6c812b10e2b1fc3c3077957df929e37988d9bd
Ali Topbaş <alitopbas10@gmail.com>
2025-07-06 13:50:05 +0300

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,6 +36,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
+    // listen for local development
     if (ENV.NODE_ENV !== 'production') {
       app.listen(ENV.PORT, () =>
         console.log('Server is up and running on PORT:', ENV.PORT)
@@ -49,4 +50,5 @@ const startServer = async () => {
 
 startServer();
 
+// export for vercel
 export default app;
