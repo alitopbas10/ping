1c2c2c48ff29a3951b4e46dbddd6713c0553f8ae
Ali Topbaş <alitopbas10@gmail.com>
2025-07-06 21:57:42 +0300

    auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from 'expo-router';
 import '../global.css';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { ClerkProvider } from '@clerk/clerk-expo';
 
 export default function RootLayout() {
-  return <Stack></Stack>;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 }
